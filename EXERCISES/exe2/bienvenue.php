41433a7cde11c23db3aec538f299ca3d6d01719d
patrickwebdev <57304731+patrickwebdev@users.noreply.github.com>
2023-05-16 10:47:22 -0400

    Create bienvenue.php

---
+++ EXERCISES/exe2/bienvenue.php
@@ -0,0 +1,15 @@
+<?php
+  //Programme de bienvenue
+?>
+<!DOCTYPE html >
+<html>
+  <head></head>
+  <body>
+    <?php
+      //Assigner des values aux variables
+      $message1='Bienvenue!';
+    ?>
+    <h1>Mon premier mot est : <?php echo $message1; //Afficher $message ?> </h1>
+  </body>
+</html>
+
