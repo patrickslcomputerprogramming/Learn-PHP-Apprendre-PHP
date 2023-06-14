77db551c447353335d55bab22eee7476bfd2794a
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2023-06-14 17:55:38 -0400

    Update index.php

--- abc123game.ca/index.php
+++ abc123game.ca/index.php
@@ -1,1 +1,9 @@
-
+<?php
+if (!isset($_SESSION)){
+    //Redirect to the login form
+    header('Location: public/form/signin-form.php'); 
+}
+else {
+    //Redirect to the appropriate game form level
+    header('Location: game.php');
+}
