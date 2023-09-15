37ac0ae5f25d2ce74c017993bbc21820d7204988
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2023-09-14 22:42:49 -0400

    Add files via upload

---
+++ EXERCISES/exe2_html&php/exe2_1_welcome.php
@@ -0,0 +1,15 @@
+<?php
+  //Welcome Program
+?>
+<!DOCTYPE html >
+<html>
+  <head></head>
+  <body>
+    <?php
+      //Assign values to variables
+      $message1='Welcome!';
+    ?>
+    <h1>My first word is: <?php echo $message1; //Afficher $message ?> </h1>
+  </body>
+</html>
+
