2059ed9deee7da5837d704201e7f72e18d2bbb5a
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2023-10-16 11:26:03 -0400

    Update demonst9_oop.php

--- DEMONSTRATION/demonst9_oop.php
+++ DEMONSTRATION/demonst9_oop.php
@@ -1,4 +1,238 @@
 <?php 
+//FRENCH BELOW
+//EXERCISE 1
+//Create a class without a Constructor Method 
+//Declare a Class
+class Students
+{
+  //Declare public Properties
+  public $firstname;
+  public $lastname;
+
+  //Declare a private method that calculates the fisrtname
+  private function setFirstname(){
+    return ucFirst(strtolower($this->firstname));
+  }
+
+  //Declare a public method that gives access to the firstname
+  public function getFirstname(){
+    return $this->setFirstname();
+  }
+
+  //Declare a private method that calculates the lastname
+  private function setLastname(){
+      return ucFirst(strtolower($this->lastname));
+  }
+  
+  //Déclarer a public method that gives access to the lastname
+  public function getLastname(){
+      return $this->setLastname();
+  }
+
+  //Declare a private method that calculates a random ID
+  private function setID()
+  {
+    $id=rand();
+    return $id; 
+  }
+ 
+  //Declare a public method that gives access to the random ID
+  public function getID()
+  {
+    return $this->setID(); 
+  }
+}
+
+//Instantiate Objets
+$student1 = new Students;
+$student2 = new Students;
+$student3 = new Students;
+
+//Access public Properties
+$student1 ->firstname = 'Patrick';
+$student1 ->lastname = 'Saint-Louis';
+
+$student2 ->firstname = 'Rasmus';
+$student2 ->lastname ='Lerdorf'; 
+
+$student3 ->firstname ='Albert';
+$student3 ->lastname = 'Einstein'; 
+
+//Access public Properties
+echo "STUDENT #1 : " . $student1 ->firstname . " " . $student1 ->lastname . "<br/>";
+echo "STUDENT #2 : " . $student2 ->firstname . " " . $student2 ->lastname . "<br/>";
+echo "STUDENT #3 : " . $student3 ->firstname . " " . $student3 ->lastname . "<br/>";
+
+//Access public Methods
+echo "STUDENT #1 : " . $student1 ->getFirstname() . " " 
+                     . $student1 ->getLastname() . " "
+                     . $student1 ->getID() . "<br/>";
+
+echo "STUDENT #2 : " . $student2 ->getFirstname() . " " 
+                     . $student2 ->getLastname() . " "
+                     . $student2 ->getID() . "<br/>";
+
+echo "STUDENT #3 : " . $student3 ->getFirstname() . " " 
+                     . $student3 ->getLastname() . " "
+                     . $student3 ->getID() . "<br/>";                
+
+
+
+
+//EXERCISE 2
+//Create a class with a Constructor Method 
+//Declare a Class
+class Students
+{
+  //Declare private Properties
+  private $firstname;
+  private $lastname;
+
+  //Declare a Constructor Method
+  public function __construct($fn, $ln){
+      $this->firstname = $fn;
+      $this->lastname = $ln;
+  }
+
+  //Declare a private method that calculates the fisrtname
+  private function setFirstname(){
+    return ucFirst(strtolower($this->firstname));
+  }
+
+  //Declare a public method that gives access to the firstname
+  public function getFirstname(){
+    return $this->setFirstname();
+  }
+
+  //Declare a private method that calculates the lastname
+  private function setLastname(){
+      return ucFirst(strtolower($this->lastname));
+  }
+  
+  //Déclarer a public method that gives access to the lastname
+  public function getLastname(){
+      return $this->setLastname();
+  }
+
+  //Declare a private method that calculates a random ID
+  private function setID()
+  {
+    $id=rand();
+    return $id; 
+  }
+ 
+  //Declare a public method that gives access to the random ID
+  public function getID()
+  {
+    return $this->setID(); 
+  }
+}
+
+//Instantiate Objets
+$student1 = new Students('Patrick','Saint-Louis');
+$student2 = new Students('Rasmus', 'Lerdorf');
+$student3 = new Students('Albert', 'Einstein');
+
+//Access public Methods
+echo "STUDENT #1 : " . $student1 ->getFirstname() . " " 
+                     . $student1 ->getLastname() . " "
+                     . $student1 ->getID() . "<br/>";
+
+echo "STUDENT #2 : " . $student2 ->getFirstname() . " " 
+                     . $student2 ->getLastname() . " "
+                     . $student2 ->getID() . "<br/>";
+
+echo "STUDENT #3 : " . $student3 ->getFirstname() . " " 
+                     . $student3 ->getLastname() . " "
+                     . $student3 ->getID() . "<br/>";  
+
+
+//EXERCISE 3
+
+//Declare a Class
+class Students
+{
+  //Declare private Properties
+  private $firstname;
+  private $lastname;
+
+  //Declare a Constructor Method
+  public function __construct($fn, $ln){
+      $this->firstname = $fn;
+      $this->lastname = $ln;
+  }
+
+  //Declare a private method that calculates the fisrtname
+  private function setFirstname(){
+    return ucFirst(strtolower($this->firstname));
+  }
+
+  //Declare a protected method that gives access to the firstname
+  public function getFirstname(){
+    return $this->setFirstname();
+  }
+
+  //Declare a private method that calculates the lastname
+  private function setLastname(){
+      return ucFirst(strtolower($this->lastname));
+  }
+  
+  //Déclarer a protected method that gives access to the lastname
+  public function getLastname(){
+      return $this->setLastname();
+  }
+
+  //Declare a private method that calculates a random ID
+  private function setID()
+  {
+    $id=rand();
+    return $id; 
+  }
+ 
+  //Declare a protected method that gives access to the random ID
+  public function getID()
+  {
+    return $this->setID(); 
+  }
+}
+
+
+//Create a sub class of Students  
+//A sub class is a "child" class and Students is its "parent" class
+class DisplayStudents extends Students
+{
+  //Declare a public Method to display output of Students
+  public function displayOutputs(){
+      echo $this ->getFirstname() . " " .
+           $this ->getLastname() . " " .
+           $this ->getID();
+  }
+}
+
+//Instantiate Objets
+$student1 = new DisplayStudents('Patrick','Saint-Louis');
+$student2 = new DisplayStudents('Rasmus', 'Lerdorf');
+$student3 = new DisplayStudents('Albert', 'Einstein');
+
+//Access public Methods
+echo "STUDENT #1 : "; 
+$student1 ->displayOutputs();
+echo "<br/>";
+
+echo "STUDENT #2 : "; 
+$student2 ->displayOutputs();
+echo "<br/>";
+
+echo "STUDENT #3 : "; 
+$student3 ->displayOutputs();
+echo "<br/>";
+
+
+
+// -----------------------------------------
+// FRENCH --------------------
+// -----------------------------------------
+
 
 //Créer une classe sans méthode constructeur
 //Déclarer la classe
