a3c71dc14fad194bfdcffd699872f8f8a2476dfe
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2024-01-17 13:50:18 -0500

    Create index.php

---
+++ Template|Modele FORM/form4/index.php
@@ -0,0 +1,23 @@
+<!--index.html-->
+<!--FORM-->
+<!--Patrick Saint-Louis, 2024-->
+ 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HTML Form</title>
+</head>
+
+<body>
+    <!--Form-->
+    <form id="form1" method="post" action="process-index.php">
+        <!--Form field to input data-->
+        <label for="i1">Name</label>
+        <input id="i1" type="text" name="user-identity"/>
+        <!--Submit button to send form data-->
+        <input id="i2" type="submit" name="form-sender" value="SEND" />
+    </form>
+</body>
+
+</html>
