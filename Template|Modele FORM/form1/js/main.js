c4a66a90177c14413b5f3a70245fd15a42f6b7c7
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2024-04-04 15:29:12 -0400

    Delete Template|Modele FORM directory

--- Template|Modele FORM/form1/js/main.js
+++
@@ -1,6 +0,0 @@
-function inputbackground() {
-    $('#inputfname').focus(function() { $(this).css('background', '#dddd88') } )
-    $('#inputfname') .blur(function() { $(this).css('background', '#ffffff') } )
-    $('#inputlname').focus(function() { $(this).css('background', '#dddd88') } )
-    $('#inputlname') .blur(function() { $(this).css('background', '#ffffff') } )
-}
