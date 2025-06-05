c229d0ffbecfc8dca5a3f7c50a7031e10717f309
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-06-05 16:10:27 -0400

    Add files via upload

---
+++ INCOMPLETE/exe7_date_fr_A_COMPLETER/afficher_date_heure.php
@@ -0,0 +1,51 @@
+<?php
+if (isset($_POST["send-button"])) {
+    //Accessible Si le fichier est accédé après la soumission du formulaire
+    //Enregistrer les données soumises dans le formulaire
+    $userContinent = $_POST["user-continent"];
+    $userCity = $_POST["user-city"];
+
+    //Charger le fichier des fonctions
+    require "fonctions.php";
+
+    //Créer un fuseau horaire (ex: "America/Montreal")
+    $fh=creerFuseauHoraire($userContinent, $userCity);
+    //Valider un fuseau horaire
+    if (validerFuseauHoraire($fh)==true){
+        //Calculer date et heure selon fuseau horaire
+        $format_dh = "l F d, Y - H:i:s";
+        $dh=calculerDateheure($fh, $format_dh);
+        $msg= "Date et Heure : " . $dh; 
+    }else{
+        $msg= "Erreur détectée dans le Fuseau Horaire. Essayez encore!";
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Affichage des résultats</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    //Afficher les résultats
+    echo "<p> Continent: " . $userContinent . "</p>";
+    echo "<p> City: " . $userCity . "</p>";
+    echo "<p><strong>" . $msg . "</strong></p>";
+    ?>
+
+    <!--Lien de retour à la page d'accueil -->
+    <button><a href="index.html">FORMULAIRE</a></button>
+</body>
+</html>
+
+<?php
+} else {
+    //Accessible si le fichier est accédé avant la soumission du formulaire
+    //Rediriger à la page d'accueil
+    header('Location: index.html'); 
+}
+?>
