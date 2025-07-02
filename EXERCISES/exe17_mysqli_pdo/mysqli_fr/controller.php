904147ea7393b66fe91ad01b18105459c5c2c10e
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-07-02 14:07:54 -0400

    Update controller.php

--- EXERCISES/exe17_mysqli_pdo/mysqli_fr/controller.php
+++ EXERCISES/exe17_mysqli_pdo/mysqli_fr/controller.php
@@ -20,22 +20,27 @@
         <hr />
         <?php
         //Activate exception 
+        //Activer les exceptions
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
         //Assign data collected from the form
+        //Assigner les données collectées via le formulaire
         $theFirstName = $_POST['prenom'];
         $theLastName = $_POST['nom'];
         $theEmail = $_POST['email'];
 
         //Login info 
+        //Information de connexion
         define('HOSTNAME', 'localhost');
         define('USERNAME', 'root');
         define('PASSWORD', '');
 
         //Load the content of the user-defined functions used to interact with MySQL
+        //Charger le contenu d'un fichier
         require_once "db_management.php";
 
         ?>
+        <!-- Link to return to the form - Lien de retour au formulaire -->
         <div id="back">
             <a href="index.php"><input type="submit" value="Essayez encore!"></a>
         </div>
