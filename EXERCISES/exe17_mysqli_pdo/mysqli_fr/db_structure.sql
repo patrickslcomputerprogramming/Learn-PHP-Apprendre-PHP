0e3f203ea4dd20af0b474f9c8eab6ca8129444b8
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-07-02 14:04:27 -0400

    Update db_structure.sql

--- EXERCISES/exe17_mysqli_pdo/mysqli_fr/db_structure.sql
+++ EXERCISES/exe17_mysqli_pdo/mysqli_fr/db_structure.sql
@@ -2,12 +2,15 @@
 -- date:2023
 
 -- 1.To create the database "users" structure
+-- 1.Pour créer la structure de la base de données
 CREATE DATABASE IF NOT EXISTS utilisateurs; 
 
 -- 2.To Use the Database 
+-- 2.Pour utiliser la base de données
 USE utilisateurs;
 
 -- 3.To create the table "employees" stucture
+-- 3.Pour créer la struture de la table
 CREATE TABLE IF NOT EXISTS employes(
     id INT(5) PRIMARY KEY AUTO_INCREMENT,
     prenom VARCHAR(35) NOT NULL,
