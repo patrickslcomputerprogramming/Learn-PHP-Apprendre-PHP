ca5e364ed6b3db7b51a3621cd3ef24c44c4cc48a
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-10-29 19:00:56 -0400

    Update db_management.php

--- EXERCISES/exe17_mysqli_pdo/mysqli/db_management.php
+++ EXERCISES/exe17_mysqli_pdo/mysqli/db_management.php
@@ -1,122 +1,59 @@
 <?php
-
-//-----------------------------------------------------------------
-//-------- FEATURE 1: CREATE THE DATABASE AND TABLE STRUCTURE -----
-//-------- DATA DEFINITION ---
-//-----------------------------------------------------------------
-
-//1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
+ 
 try {
-    $connection = new mysqli(HOSTNAME, USERNAME, PASSWORD);
-} catch (mysqli_sql_exception $error) {
-    //If the connection failed, display error message and stop the script
-    die("Connection to MySQL failed! <br>" . $error);
-}
-
-//2-CREATE THE DATABASE STRUCTURE IF IT DOESN'T EXIST YET
-try {
-    $createStructure = $connection->multi_query(file_get_contents("db_structure.sql"));
-} catch (mysqli_sql_exception $error) {
-    //If the creation failed, display error message and stop the script
-    die("Creation of Database and Tables failed! <br>" . $error);
-}
-
-
-//3-DISCONNECT FROM THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
-try {
+    //-----------------------------------------------------------------
+    //-------- FEATURE 1: CREATE THE DATABASE AND TABLE STRUCTURE -----
+    //-------- DATA DEFINITION ---
+    //-----------------------------------------------------------------
+    //1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
+    $connection = new mysqli(hostname:HOSTNAME, username:USERNAME, password:PASSWORD);
+    //2-CREATE THE DATABASE STRUCTURE IF IT DOESN'T EXIST YET USING THE CODE IN AN EXTERNAL FILE
+    $connection->multi_query(query: file_get_contents(filename: "db_structure.sql"));
+    //3-DISCONNECT FROM THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
     $connection->close();
-} catch (mysqli_sql_exception $error) {
-    //If the disconnection failed, display error message and stop the script
-    die("Disconnection from MySQL failed!<br/>" . $error);
-}
-
-
-//-----------------------------------------------------------------
-//-------- FEATURE 2: INSERT 1 RECORD -----------------------------
-//-------- DATA UPDATE --------------------------
-//-----------------------------------------------------------------
-
-
-//1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
-try {
+    //-----------------------------------------------------------------
+    //-------- FEATURE 2: INSERT 1 RECORD -----------------------------
+    //-------- DATA UPDATE --------------------------
+    //-----------------------------------------------------------------
+    //1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
     $connection = new mysqli(HOSTNAME, USERNAME, PASSWORD);
-} catch (mysqli_sql_exception $error) {
-    //If the connection failed, display error message and stop the script
-    die("Connection to MySQL failed! <br>" . $error);
-}
-
-
-//2-SELECT THE DATABASE
-try {
-    $selectDBUsers = mysqli_select_db($connection, "users");
-} catch (mysqli_sql_exception $error) {
-    //If the selection failed, display error message and stop the script
-    die("Connection to the Database failed!<br/> " . $error);
-}
-
-
-//3-CHECK IF THE TABLE EXISTS 
-try {
+    //2-SELECT THE DATABASE
+    $sqlCode = "USE users";
+    $connection->query(query: $sqlCode);
+    //3-CHECK IF THE TABLE EXISTS 
     $sqlCode = "DESC employees";
-    $describeTable = $connection->query($sqlCode);
-} catch (mysqli_sql_exception $error) {
-    //If the description failed, display error message and stop the script
-    die("Description of the Table failed!<br/> " . $error);
-}
-
-//4-INSERT A RECORD INTO THE TABLE 
-try {
-    $sqlCode = "INSERT INTO employees (firstname, lastname, email) 
-                VALUES ('$theFirstName', '$theLastName', '$theEmail')";
-    $insertRecords = $connection->query($sqlCode);
-} catch (mysqli_sql_exception $error) {
-    //If the insertion failed, display error message and stop the script
-    die("Data insertion into the Table failed!<br>" . $error);
-}
-
-//5-DISCONNECT FROM THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
-try {
-    $disconnection = $connection->close();
-} catch (mysqli_sql_exception $error) {
-    //If the disconnection failed, display error message and stop the script
-    die("Disconnection from MySQL failed!<br/>" . $error);
-}
-
-//-----------------------------------------------------------------
-//-------- FEATURE 3: SELECT ALL RECORDS ---------------
-//-------- DATA RETRIEVAL --------------------------
-//-------- DISPLAY THE RECORDS SELECTED --------------------------
-//-----------------------------------------------------------------
-
-//1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
-try {
+    $connection->query(query: $sqlCode);
+    //4-INSERT A RECORD INTO THE TABLE    
+    /*
+    //Regular statement
+    $sqlCode = "INSERT INTO employees (firstname, lastname, email) VALUES ('$theFirstName', '$theLastName', '$theEmail')";
+    $connection->query($sqlCode);
+    */
+    //Prepared statement
+    $sqlCode = "INSERT INTO employees (firstname, lastname, email) VALUES (?, ?, ?)";        
+    $prepStat = $connection->prepare($sqlCode);
+    $prepStat->bind_param('sss', $theFirstName, $theLastName, $theEmail); //s for string ; d for double ; i for int...
+    $prepStat->execute();
+    //echo $prepStat->affected_rows;
+    //5-DISCONNECT FROM THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
+    //Close the prepared statement object 
+    $prepStat->close();
+    //Close the mysql object
+    $connection->close();
+    //-----------------------------------------------------------------
+    //-------- FEATURE 3: SELECT ALL RECORDS ---------------
+    //-------- DATA RETRIEVAL --------------------------
+    //-------- DISPLAY THE RECORDS SELECTED --------------------------
+    //-----------------------------------------------------------------
+    //1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
     $connection = new mysqli(HOSTNAME, USERNAME, PASSWORD);
-} catch (mysqli_sql_exception $error) {
-    //If the connection failed, display error message and stop the script
-    die("Connection to MySQL failed! <br>" . $error);
-}
-
-
-//2-SELECT THE DATABASE
-try {
-    $selectDBUsers = mysqli_select_db($connection, "users");
-} catch (mysqli_sql_exception $error) {
-    //If the selection failed, display error message and stop the script
-    die("Connection to the Database failed!<br/> " . $error);
-}
-
-
-//3-CHECK IF THE TABLE EXISTS 
-try {
+    //2-SELECT THE DATABASE
+    $sqlCode = "USE users";
+    $connection->query(query: $sqlCode);
+    //3-CHECK IF THE TABLE EXISTS 
     $sqlCode = "DESC employees";
-    $describeTable = $connection->query($sqlCode);
-} catch (mysqli_sql_exception $error) {
-    //If the description failed, display error message and stop the script
-    die("Description of the Table failed!<br/> " . $error);
-}
-
-//4-SELECT ALL THE EXISTING RECORDS INTO THE TABLE AND DISPLAY THEM
-try {
+    $connection->query(query: $sqlCode);
+    //4-SELECT ALL THE EXISTING RECORDS INTO THE TABLE AND DISPLAY THEM
     $sqlCode = "SELECT * FROM employees";
     $selectRecords = $connection->query($sqlCode);
     //Calculate the number of records (or rows) available
@@ -136,15 +73,21 @@ try {
         echo "</tr>";
     }
     echo "</table>";
+    //5-DISCONNECT FROM THE RESULT SET AND THE DATABASE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
+    $selectRecords->close();
+    $connection->close();
 } catch (mysqli_sql_exception $error) {
-    //If the selection failed, display error message and stop the script
-    die("Data selection from the Table failed!<br/>" . $error);
-}
-
-//5-DISCONNECT FROM THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
-try {
-    $disconnection = $connection->close();
-} catch (mysqli_sql_exception $error) {
-    //If the disconnection failed, display error message and stop the script
-    die("Disconnection from MySQL failed!<br/>" . $error);
+    //if there an error 
+    $msg_erreur['Customized'] = "Something went wrong! Try again!";
+    $msg_erreur['Error Message'] = $error->getMessage();
+    $msg_erreur['Error File Name'] = $error->getFile();
+    $msg_erreur['Error Line Number'] = $error->getLine();
+    echo "<table>";
+    foreach ($msg_erreur as $key => $value){
+        echo "<tr>";
+        echo "<td>" . $key . "</td>" ;
+        echo "<td>" . $value . "</td>" ;
+        echo "</tr>";
+    }
+    echo "</tr></table>";
 }
