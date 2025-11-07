dabffa54f288b129153070607ddd73f08fc4c31e
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2025-11-07 17:06:41 -0500

    Update get_regions.php

--- EXERCISES/exe18_Ajax_DB/exe18_autocomplete_ajax_db/exe18_autocomplete_ajax_db_ver2/get_regions.php
+++ EXERCISES/exe18_Ajax_DB/exe18_autocomplete_ajax_db/exe18_autocomplete_ajax_db_ver2/get_regions.php
@@ -1,113 +1,95 @@
 <?php
-//Conduct error handling to understand error when there are
-//In this exercise there is no plece to display the error retrieved
-//That's why $errorMsg is not calculated everywhere, neither used
+//If the XMLHTTPRequest javascript class sent a request 
+if (isset($_REQUEST["rqst"])) {
+    //Login info 
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
 
-//STEP 1 - RECEIVE AJAX REQUEST (THIS MEANS THE CHARACTERS TO FIND IN THE BEGINNING OF THE NAMES)
-// get the q parameter from URL
-$q = $_REQUEST["rqst"];
+    //DB Info
+    $dbname = "infoqc";
+    $tablename = "regions";
+    $columname = "name";
+    $result = array();
+    $user_data = $_REQUEST["rqst"];
 
+    //CREATE THE DATABASE IF IT DOESN'T EXIST YET
+    try {
 
-//STEP 2 - RETRIEVE DATA FROM THE DATABASE
-//Login info 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
+        //1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
+        $connection = new PDO(dsn: "mysql:host=" . HOST, username: USER, password: PASS);
+        //2-CREATE TGE DATABASE 
+        $connection->exec(statement: file_get_contents("regions_administartives_qc.sql"));
+        //3-DISCONNECT FROM THE RESULT SET AND THE DATABASE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
+        unset($connection);
+    } catch (PDOException $errorPDO) {
+        //if there an error 
+        $msg_erreur['Customized'] = "Something went wrong! Try again!";
+        $msg_erreur['Error Message'] = $errorPDO->getMessage();
+        $msg_erreur['Error File Name'] = $errorPDO->getFile();
+        $msg_erreur['Error Line Number'] = $errorPDO->getLine();
+        echo "<table>";
+        foreach ($msg_erreur as $key => $value) {
+            echo "<tr>";
+            echo "<td>" . $key . "</td>";
+            echo "<td>" . $value . "</td>";
+            echo "</tr>";
+        }
+        echo "</tr></table>";
+    }
 
-//DB Info
-$dbname = "infoqc";
-$tablename = "regions";
+    //RETRIEVE DATA FROM THE DATABASE
+    try {
 
-//1-Connect to the DBMS MySQL
-//1-Se connecter au SGBD MySQL
-try {
-    $connection = new mysqli(HOST, USER, PASS);
-} catch (mysqli_sql_exception $errorMysqli) {
-    $errorMsg = "Something went wrong" . $errorMysqli;
-}
-
-//2-Select the DB when it already exists
-//Or create the DB if it doesn't exist yet 
-//2-Selectionner la DB si elle existe
-//Ou creer la DB si elle n'exite pas encore
-try {
-    $sqlCode = "USE $dbname";
-    $connection->query($sqlCode);
-} catch (mysqli_sql_exception) {
-    //Hope the creation will work successfully, no error handling is conducted
-    //Conduct error handling to make sure there's no error
-    $connection->multi_query(file_get_contents("createDBStructureInsert.sql")); 
-}
-
-//3-DisConnect from the DBMS MySQL
-//3-Se deconnecter du SGBD MySQL
-try {
-    $connection->close();
-} catch (mysqli_sql_exception $errorMysqli) {
-    $errorMsg = "Something went wrong" . $errorMysqli;
-}
-
-// --------------------------------------------------
-
-//1-Connect to the DBMS MySQL
-//1-Se connecter au SGBD MySQL
-try {
-    $connection = new mysqli(HOST, USER, PASS);
-} catch (mysqli_sql_exception $errorMysqli) {
-    $errorMsg = "Something went wrong" . $errorMysqli;
-}
-
-//2-Select the DB when it already exists
-//Or create the DB if it doesn't exist yet 
-//2-Selectionner la DB si elle existe
-//Ou creer la DB si elle n'exite pas encore
-try {
-    $sqlCode = "USE $dbname";
-    $connection->query($sqlCode);
-} catch (mysqli_sql_exception) {
-    //Hope the creation will work successfully, no error handling is conducted
-    //Conduct error handling to make sure there's no error
-   $errorMsg = "Something went wrong" . $errorMysqli; 
-}
-
-//3-Select all columns of the TABLE regions
-//3-Sélectionner toutes les colonnes de la TABLE regions
-try {
-    $sqlCode = "SELECT * FROM `$tablename`;";
-    $resultSet = $connection->query($sqlCode);
-} catch (mysqli_sql_exception $errorMysqli) {
-    $errorMsg = "Something went wrong" . $errorMysqli;
-}
-
-//STEP 3 - SELECT NAMES THAT STARTS WITH THE CHARACTERS RECEIVED IN THE REQUEST
-$correspondingName = "";
-// lookup all hints from array if $q is different from ""
-if ($q !== "") {
-    $q = strtolower($q);
-    $len = strlen($q);
-    //4-Retrieve the result set returned by the Select Query 
-    foreach ($resultSet as $each_row) {
-        foreach ($each_row as $columnName => $regionName) {
-            //Compare each region name retrieved fron the db with the data received in the request
-            if (stristr($q, substr($regionName, 0, $len))) {
-                if ($correspondingName === "") {
-                    $correspondingName = $regionName;
-                } else {
-                    $correspondingName .= ", $regionName";
-                }
+        //1-CONNECT TO THE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL 
+        $connection = new PDO(dsn: "mysql:host=" . HOST, username: USER, password: PASS);
+        //2-SELECT THE DATABASE
+        $queryString = "USE $dbname";
+        $connection->query(query: $queryString);
+        //3-CHECK IF THE TABLE EXISTS 
+        $queryString = "DESC $tablename";
+        $connection->query(query: $queryString);
+        //4-SELECT ALL THE EXISTING RECORDS INTO THE TABLE AND DISPLAY THEM
+        $queryString = "SELECT " . $columname . " FROM " . $tablename . " WHERE " . $columname . " LIKE '" . $user_data . "%'";
+        $selectRecords = $connection->prepare($queryString);
+        $selectRecords->execute();
+        //Calculate the number of records (or rows) available
+        $number_of_rows = $selectRecords->rowCount();
+        $corresponding_region_names = [];
+        //Use a loop to display the records one by one in a HTML table
+        for ($j = 0; $j < $number_of_rows; ++$j) {
+            $row = $selectRecords->fetch(PDO::FETCH_NUM);
+            foreach ($row as $key => $value) {
+                $corresponding_region_names[] = $value;
             }
         }
+        //5-DISCONNECT FROM THE RESULT SET AND THE DATABASE DATABASE MANAGEMENT SYSTEM (DBMS) MYSQL
+        unset($selectRecords);
+        unset($connection);
+    } catch (PDOException $errorPDO) {
+        //if there an error 
+        $msg_erreur['Customized'] = "Something went wrong! Try again!";
+        $msg_erreur['Error Message'] = $errorPDO->getMessage();
+        $msg_erreur['Error File Name'] = $errorPDO->getFile();
+        $msg_erreur['Error Line Number'] = $errorPDO->getLine();
+        echo "<table>";
+        foreach ($msg_erreur as $key => $value) {
+            echo "<tr>";
+            echo "<td>" . $key . "</td>";
+            echo "<td>" . $value . "</td>";
+            echo "</tr>";
+        }
+        echo "</tr></table>";
     }
-}
 
-//5-DisConnect from the DBMS MySQL
-//5-Se deconnecter du SGBD MySQL
-try {
-    $connection->close();
-} catch (mysqli_sql_exception $errorMysqli) {
-    $errorMsg = "Something went wrong" . $errorMysqli;
-}
 
-//STEP 4 - ANSWER THE REQUEST (THIS MEANS SEND THE NAMES FOUND) 
-// Output hints when there are, or nothing when there aren't
-echo $correspondingName;
+    //Display outputs (send a response to the XMLHTTPRequest JavaScript class request)
+    unset($_REQUEST["rqst"]);
+    echo count(value: $corresponding_region_names) == 0 ?  "" : implode(separator: ", ", array: $corresponding_region_names);
+} 
+//If the XMLHTTPRequest javascript class didn't sent a request 
+else {
+    //Redirect to the home page 
+    header(header: "Location: index.php");
+    exit();
+}
