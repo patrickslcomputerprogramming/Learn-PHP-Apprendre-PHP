f847ecc6ef3cef0a335efeeddccf33ee81dd325a
Patrick Saint-Louis <patricksaintlouis1@gmail.com>
2026-02-06 09:00:58 -0500

    Update afficher_date_heure.php

--- INCOMPLETE/exe7_date_fr_A_COMPLETER/afficher_date_heure.php
+++ INCOMPLETE/exe7_date_fr_A_COMPLETER/afficher_date_heure.php
@@ -2,50 +2,58 @@
 if (isset($_POST["send-button"])) {
     //Accessible Si le fichier est accédé après la soumission du formulaire
     //Enregistrer les données soumises dans le formulaire
-    $userContinent = $_POST["user-continent"];
-    $userCity = $_POST["user-city"];
+    $continent = $_POST["user-continent"];
+    $ville = $_POST["user-city"];
 
     //Charger le fichier des fonctions
-    require "fonctions.php";
+    require_once "fonctions.php";
 
-    //Créer un fuseau horaire (ex: "America/Montreal")
-    $fh=creerFuseauHoraire($userContinent, $userCity);
-    //Valider un fuseau horaire
-    if (validerFuseauHoraire($fh)==true){
-        //Calculer date et heure selon fuseau horaire
-        $format_dh = "l F d, Y - H:i:s";
-        $dh=calculerDateheure($fh, $format_dh);
-        $msg= "Date et Heure : " . $dh; 
-    }else{
-        $msg= "Erreur détectée dans le Fuseau Horaire. Essayez encore!";
+    //Fonction de synthèse pour implémenter la fonctionnalité
+    function afficherDateEtHeure($continent, $ville): string
+    {
+        //Créer un fuseau horaire (ex: "America/Montreal")
+        $fh = creerFuseauHoraire(continent: $continent, ville: $ville);
+        //Valider le fuseau horaire
+        if (validerFuseauHoraire(fuseauHoraire: $fh) == true) {
+            //Calculer date et heure selon le fuseau horaire
+            $format_dh = "l F d, Y - H:i:s";
+            $dh = calculerDateheure(fuseauHoraire: $fh, formatAffichage: $format_dh);
+            $msg = "Date et Heure : " . $dh;
+        } else {
+            $msg = "Erreur détectée dans le Fuseau Horaire. Essayez encore!";
+        }
+        return $msg;
     }
 ?>
 
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <title>Affichage des résultats</title>
+    <head>
     <meta charset="UTF-8">
-</head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab8-Exe1-Calculateur de date-Calcul et Sorties</title>
+    </head>
 
-<body>
-    <?php
-    //Afficher les résultats
-    echo "<p> Continent: " . $userContinent . "</p>";
-    echo "<p> City: " . $userCity . "</p>";
-    echo "<p><strong>" . $msg . "</strong></p>";
-    ?>
+    <body>
+        <?php
+        //Afficher les résultats
+        echo "<p> Continent: " . $continent . "</p>";
+        echo "<p> City: " . $ville . "</p>";
+        echo "<p><strong>" . afficherDateEtHeure(continent: $continent, ville: $ville) . "</strong></p>";
+        ?>
 
-    <!--Lien de retour à la page d'accueil -->
-    <button><a href="index.html">FORMULAIRE</a></button>
-</body>
-</html>
+        <!--Lien de retour à la page d'accueil -->
+        <button><a href="index.html">FORMULAIRE</a></button>
+    </body>
+
+    </html>
 
 <?php
 } else {
     //Accessible si le fichier est accédé avant la soumission du formulaire
     //Rediriger à la page d'accueil
-    header('Location: index.html'); 
+    header('Location: index.html');
 }
 ?>
